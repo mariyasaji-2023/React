b00b06b49c64201490e894551feadbd0580386ae
mariyasaji-2023 <mariyasajichiramel@gmail.com>
2024-05-10 11:42:54 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
 import React from "react"
 import axios from "axios"
+import { useState } from "react"
 
 function App() {
+  const [state,setState] = useState([])
   return (
     <div>
       <h1>Hello world</h1>
       <button onClick={()=>{
         axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
           console.log(response.data);
+          setState(response.data)
         })
       }}>Clic Me</button>
+      {state.map((obj,index)=>{
+        return (
+          <div>
+            <h1>{index+1}</h1>
+            <h1>{obj.title}</h1>
+            <h4>{obj.body}</h4>
+          </div>
+        )
+      })}
     </div>
   )
 }
